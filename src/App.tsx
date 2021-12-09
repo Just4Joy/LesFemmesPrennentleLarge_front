9ea03abb1ffc9a7a9b47a50ccf80f13efec6e177
Koldo Etxegarai <etxegaraikoldo@gmail.com>
2021-12-09 14:59:17 +0100

    structuring project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css';
 import React from 'react';
-import BecomeWahine from './components/Home/BecomeWahine';
+import Home from './components/Home/Home';
+import Profile from './components/Profile/Profile';
+import Header from './components/Header';
+import Sessions from './components/Sessions.tsx/Sessions';
+import Footer from './components/Footer';
+
 
 function App() {
 
   return (
     <div className="App">
-      <BecomeWahine />
+      
     </div>
   );
 }
