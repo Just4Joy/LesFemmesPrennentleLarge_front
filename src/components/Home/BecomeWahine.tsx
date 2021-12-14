fa1a7ecafd2da221bb2d4648626e311603c9712b
Koldo Etxegarai <etxegaraikoldo@gmail.com>
2021-12-14 16:56:12 +0100

    paramètres généraux et seconde moitiée de home page

--- src/components/Home/BecomeWahine.tsx
+++ src/components/Home/BecomeWahine.tsx
@@ -1,13 +1,24 @@
-import React from "react";
-import "./BecomeWahine.scss";
+import React from 'react';
+import './BecomeWahine.scss';
+import becomeWahine from '../img/become-wahine.svg';
 
 const BecomeWahine = () => {
-
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className="become-wahine-container">
+      <h2 className="become-wahine-container-h2">Envie d'accompagner des surfeuses ?</h2>
+      <h3 className="become-wahine-container-h3">
+        Créer des sessions dans tes spots préférés
+      </h3>
+      <div className="become-wahine-container-row">
+        <button className="become-wahine-container-row-btn">Devenir wahine</button>
+        <img
+          className="become-wahine-container-row-img"
+          src={becomeWahine}
+          alt="Become wahine"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default BecomeWahine;
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React from 'react';
+import Wahine from '../Wahine';
+import './Home.scss';
+import { BsBoxArrowInUpRight } from 'react-icons/bs';
+import BecomeWahine from './BecomeWahine';
 
 const Home = () => {
-
-    return (
-        <div>
-
+  return (
+    <div className="home-container">
+      {/* Section : Nos wahines */}
+      <div className="home-container-wahine">
+        <h1 className="home-container-wahine-h1">Nos Wahines</h1>
+        <div className="home-container-wahine-component">
+          <Wahine />
+          <Wahine />
+          <Wahine />
+          <Wahine />
+          <Wahine />
         </div>
-    )
-}
+        <p className="home-container-wahine-p">
+          Toutes les wahines <BsBoxArrowInUpRight />
+        </p>
+      </div>
+      {/* Section : Devenir wahine */}
+      <BecomeWahine />
+    </div>
+  );
+};
 
 export default Home;
