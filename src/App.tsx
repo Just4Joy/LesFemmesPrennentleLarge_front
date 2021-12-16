2cfb9390337c1bfdf9bdd37b4be4aa8183ba5136
ChleaBourtoule <chlea.bourtoule@gmail.com>
2021-12-16 15:32:44 +0100

    router and routes done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.scss';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Footer from './components/Footer';
 import Header from './components/Header';
