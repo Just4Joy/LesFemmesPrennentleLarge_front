a5e8a4414335eebd3a854e1a6715eeee836f7eb6
marie5060 <marie5060hotmail.com>
2022-01-09 20:24:01 +0100

    modal creation profil2 scss sans map

--- src/components/Modals/CreateProfil2.tsx
+++ src/components/Modals/CreateProfil2.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import { Dispatch, SetStateAction } from 'react';
 import { NavLink } from 'react-router-dom';
-
+import '../../style/_CreateProfil2.scss';
 import SurfSkill from '../SurfSkill';
 
 type CreateProfil2Props = {
@@ -10,24 +10,35 @@ type CreateProfil2Props = {
 
 const CreateProfil2: FC<CreateProfil2Props> = ({ setActiveModal }) => {
   return (
-    <div className="CreateProfil2">
-      <div className="CreateProfil2__titles">
-        <h3>Compléter son profil 2/2</h3>
-        <h3>Skip</h3>
+    <div className="createProfil2">
+      <div className="createProfil2__titles">
+        <h2>Compléter son profil 2/2</h2>
+        <h2>Skip</h2>
       </div>
-      <div className="CreateProfil__skills">
-        <div className="CreateProfil__skills__titre">
-          <h3> Choisis tes skills</h3>
+      <div className="createProfil2__skills">
+        <div className="createProfil2__skills__titre">
+          <p> Choisis tes skills</p>
         </div>
-        <div className="CreateProfil__skills__tags">
+        <div className="createProfil2__skills__tags">
+          <SurfSkill />
+          <SurfSkill />
+          <SurfSkill />
+          <SurfSkill />
+          <SurfSkill />
+          <SurfSkill />
+          <SurfSkill />
+          <SurfSkill />
+          <SurfSkill />
+          <SurfSkill />
           <SurfSkill />
         </div>
-        <div className="CreateProfil__button">
-          <div>
-            <button onClick={() => setActiveModal('')}>
-              <NavLink to="/profile"> Valider mon profil </NavLink>
-            </button>
-          </div>
+        <div className="createProfil2__button">
+          <NavLink
+            className="createProfil2__button__valider"
+            to="/profile"
+            onClick={() => setActiveModal('')}>
+            Valider mon profil
+          </NavLink>
         </div>
       </div>
     </div>
