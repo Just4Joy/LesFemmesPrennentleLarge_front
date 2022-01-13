0cade57cbb4b05387101d638d014c18e9f01eaf4
ChleaBourtoule <chlea.bourtoule@gmail.com>
2022-01-13 16:23:34 +0100

    get surfstyles on hiki

--- src/components/Hiki.tsx
+++ src/components/Hiki.tsx
@@ -1,6 +1,7 @@
-import React, { Dispatch, FC, SetStateAction } from 'react';
+import React, { Dispatch, FC, SetStateAction, useEffect, useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
-
+import ISurfStyle from '../interfaces/ISurfStyle';
 import wahine from '../../img/wahine.svg';
 import IUser from '../interfaces/IUser';
 
@@ -12,9 +13,16 @@ const Hiki: FC<Props> = ({
   lastname,
   city,
   favorite_spot,
-  surf_style,
   id_user,
+  id_surf_style,
 }) => {
+  const [surfStyles, setSurfStyles] = useState<ISurfStyle>();
+  useEffect(() => {
+    axios
+      .get<ISurfStyle>(`http://localhost:3000/api/surfstyle/${id_surf_style}`)
+      .then((result) => result.data)
+      .then((data) => setSurfStyles(data));
+  }, []);
   return (
     <Link to={`/${id_user}`}>
       <div
@@ -31,7 +39,7 @@ const Hiki: FC<Props> = ({
         </h5>
         <h6 className="">{city}</h6>
         <h6 className="hiki__h6">{favorite_spot}s</h6>
-        <h6 className="hiki__tag">{surf_style}</h6>
+        <h6 className="hiki__tag">{surfStyles?.name_user}</h6>
       </div>
     </Link>
   );
