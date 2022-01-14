5fc9b408aefd42e98b60a81b602e8efe5a22e96d
ChleaBourtoule <chlea.bourtoule@gmail.com>
2022-01-14 10:44:06 +0100

    route chaanged for  wahine2

---
+++ src/components/Wahine2.tsx
@@ -0,0 +1,39 @@
+import React, { Dispatch, FC, SetStateAction } from 'react';
+import { Link } from 'react-router-dom';
+
+import wahineImg from '../../img/wahine.svg';
+import IUser from '../interfaces/IUser';
+
+type Props = IUser & { setActiveModal: Dispatch<SetStateAction<string>> };
+
+const Wahine: FC<Props> = ({
+  setActiveModal, // profile_pic,
+  firstname,
+  lastname,
+  city,
+  favorite_spot,
+  id_user,
+}) => {
+  return (
+    <Link to={`/session/${id_user}`}>
+      <div
+        role="presentation"
+        className="wahine"
+        style={{ cursor: 'pointer' }}
+        onClick={() => setActiveModal('wahine2')}
+        key={id_user}>
+        <div className="wahine__img">
+          <img className="" /*src={profile_pic}*/ src={wahineImg} alt="wahine" />
+        </div>
+
+        <h5 className="wahine__h5">
+          {firstname} {lastname}
+        </h5>
+        <h6 className="">{city}</h6>
+        <h6 className="wahine__h6">{favorite_spot}</h6>
+      </div>
+    </Link>
+  );
+};
+
+export default Wahine;
