232136eafd2cfd9e68073d66638b10692fa9384a
ChleaBourtoule <chlea.bourtoule@gmail.com>
2022-01-17 17:11:19 +0100

    route to surfskills modified again

--- src/components/Profile/SurfSkillProfile.tsx
+++ src/components/Profile/SurfSkillProfile.tsx
@@ -1,25 +1,12 @@
-import axios from 'axios';
-import React, { FC, useState } from 'react';
-import { useEffect } from 'react';
+import React, { FC } from 'react';
 
 import ISurfSkill from '../../interfaces/ISurfskills';
-import IUserhassurfskills from '../../interfaces/IUserHasSurfSkill';
 
-type Props = IUserhassurfskills;
-const SurfSkillProfile: FC<Props> = ({ id_surf_skill }) => {
-  const [surfSkillName, setSurfSkillName] = useState<ISurfSkill>();
-
-  useEffect(() => {
-    //Get Surf SKills name
-    axios
-      .get<ISurfSkill>(`http://localhost:3000/api/surfskill/${id_surf_skill}`)
-      .then((result) => result.data)
-      .then((data) => setSurfSkillName(data));
-  }, []);
-
+type Props = ISurfSkill;
+const SurfSkillProfile: FC<Props> = ({ ...surfSkill }) => {
   return (
     <div className="surfskillprofile">
-      <p>{surfSkillName?.name}</p>
+      <p>{surfSkill.name}</p>
     </div>
   );
 };
