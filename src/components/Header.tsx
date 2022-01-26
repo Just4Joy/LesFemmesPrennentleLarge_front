36f253893624845248f082e4bdbd46d800b1697d
ChleaBourtoule <chlea.bourtoule@gmail.com>
2022-01-26 12:03:50 +0100

    header dropdown on profile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import React, { FC, useContext } from 'react';
+// import { useRef, useEffect } from 'react';
+import { useState } from 'react';
 import { Dispatch, SetStateAction } from 'react';
-import { IoIosArrowDown } from 'react-icons/io';
+import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
 import { NavLink } from 'react-router-dom';
 
 import LFPLL from '../../img/LFPLL.svg';
@@ -12,6 +14,27 @@ type Props = {
 
 const Header: FC<Props> = ({ setActiveModal }) => {
   const { id, logout, wahine } = useContext(CurrentUserContext);
+  const [dropDown, setDropDown] = useState(false);
+  // const ref = useRef();
+
+  // useEffect(() => {
+  //   const checkIfClickedOutside = (e: any) => {
+  //     // If the menu is open and the clicked target is not within the menu,
+  //     // then close the menu
+  //     // @ts-ignore: Unreachable code error
+  //     if (dropDown && ref.current && !ref.current.contains(e.target)) {
+  //       setDropDown(false);
+  //     }
+  //   };
+
+  //   document.addEventListener('mousedown', checkIfClickedOutside);
+
+  //   return () => {
+  //     // Cleanup the event listener
+  //     document.removeEventListener('mousedown', checkIfClickedOutside);
+  //   };
+  // }, [dropDown]);
+
   return (
     <div className="header">
       <div className="header__logo">
@@ -24,22 +47,56 @@ const Header: FC<Props> = ({ setActiveModal }) => {
           {id === 0 ? (
             ''
           ) : (
-            <NavLink to="/profile">
-              Profil
-              <IoIosArrowDown />
-            </NavLink>
+            // <NavLink to="/profile">
+            //   Profil
+            //   <IoIosArrowDown />
+            // </NavLink>
+            <div
+              className={
+                dropDown
+                  ? 'header__list__profile__dropdown2'
+                  : 'header__list__profile__dropdown'
+              }>
+              <p
+                className="header__list__profile__dropdown__drop"
+                role="presentation"
+                onClick={() => setDropDown(!dropDown)}>
+                Profile
+                {dropDown ? (
+                  <IoIosArrowUp className="header__list__profile__dropdown__drop__arrow" />
+                ) : (
+                  <IoIosArrowDown className="header__list__profile__dropdown__drop__arrow" />
+                )}
+              </p>
+              {dropDown ? (
+                <div className="header__list__profile__dropdown__links">
+                  <NavLink to="/profile">
+                    <li
+                      className="header__list__profile__dropdown__links__link"
+                      role="presentation"
+                      onClick={() => setDropDown(false)}>
+                      My Profile
+                    </li>
+                  </NavLink>
+                  <NavLink to="/my_sessions">
+                    <li
+                      className="header__list__profile__dropdown__links__link"
+                      role="presentation"
+                      onClick={() => setDropDown(false)}>
+                      Mes Sessions
+                    </li>
+                  </NavLink>
+                </div>
+              ) : (
+                ''
+              )}
+            </div>
           )}
         </li>
         <li className="header__list__session">
           <NavLink to="/sessions">Sessions</NavLink>
         </li>
-        {wahine ? (
-          <li className="header__list__create">
-            <NavLink to="/my_sessions">Mes sessions</NavLink>
-          </li>
-        ) : (
-          ''
-        )}
+
         <li className="header__list__connection">
           {id === 0 ? (
             <NavLink
@@ -50,12 +107,14 @@ const Header: FC<Props> = ({ setActiveModal }) => {
               Se connecter
             </NavLink>
           ) : (
-            <span
-              className="header__list__connection__logout"
-              role="presentation"
-              onClick={() => logout()}>
-              Se déconnecter
-            </span>
+            <NavLink to="/">
+              <span
+                className="header__list__connection__logout"
+                role="presentation"
+                onClick={() => logout()}>
+                Se déconnecter
+              </span>
+            </NavLink>
           )}
         </li>
         {wahine ? (
